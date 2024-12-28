5e7ffc44a85e4237134f63e6d1827972cfa87632
samir1maity <s879maity@gmail.com>
2024-12-29 01:25:34 +0530

    done gemini setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,40 @@
-import express from 'express'
+import express from "express";
+import "dotenv/config";
+import {
+  GoogleGenerativeAI,
+  HarmCategory,
+  HarmBlockThreshold,
+} from "@google/generative-ai";
 
 const app = express()
 
-app.listen(3000, ()=>{
-    console.log('be running')
-    
-})
+const apiKey = process.env.GEMINI_API_KEY;
+const genAI = new GoogleGenerativeAI(apiKey as string);
+
+const model = genAI.getGenerativeModel({
+  model: "gemini-1.5-flash",
+});
+
+const generationConfig = {
+  temperature: 1,
+  topP: 0.95,
+  topK: 40,
+  maxOutputTokens: 8192,
+  responseMimeType: "text/plain",
+};
+
+async function run() {
+  const chatSession = model.startChat({
+    generationConfig,
+    history: [],
+  });
+
+  const result = await chatSession.sendMessage("INSERT_INPUT_HERE");
+  console.log(result.response.text());
+
+  app.listen('3000', ()=>{
+    console.log('server running at 3000')
+  })
+}
+
+run();
